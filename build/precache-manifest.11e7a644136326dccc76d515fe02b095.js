b8d745d37fb6e9a1a18596cdf0435ed29822bbf7
Ross Hutchens <rlhcxm@gmail.com>
2020-07-29 16:21:47 -0500

    Deployed app to Heroku

---
+++ build/precache-manifest.11e7a644136326dccc76d515fe02b095.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0e9b7b65fb81cebf5bf777b3ddeb07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30d9e23987204a1c1595",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "6c1cbdccc6a517fe0f05",
+    "url": "/static/js/2.79b471ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.79b471ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d9e23987204a1c1595",
+    "url": "/static/js/main.90994a32.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
